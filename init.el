d35a22fdd94bfaf653ea7910c1e7d568edfa3035
Aqeel Akber <aqeel.akber@gmail.com>
2017-09-08 10:32:06 +1000

    initial readme

--- init.el
+++ init.el
@@ -53,3 +53,17 @@
 
 ;; Auto appended from here on
 ;; ------------------------------------------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (irony flycheck company yasnippet tablist helm flycheck-irony company-irony base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
